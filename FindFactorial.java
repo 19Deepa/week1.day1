fd4b6f611f20d0dce6bb284cf3ba6a1408b8105f
19Deepa <107349074+19Deepa@users.noreply.github.com>
2022-06-16 12:36:46 +0530

    Add files via upload

---
+++ FindFactorial.java
@@ -0,0 +1,15 @@
+package week1.Day1;
+
+public class FindFactorial {
+	
+   public static void main(String[] args) {
+		int i= 5, fact =1;
+		for (int j = 1; j <=i;j++) {
+			 fact =fact*j;
+			}
+		System.out.println(fact);
+		
+		
+	}
+
+}
